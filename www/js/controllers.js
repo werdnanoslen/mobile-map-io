c1eecf4216d5fa9a487e9a5b90b3e50093189ddf
Andrew Nelson <andy@andyhub.com>
2015-08-27 22:57:53 -0400

    Added more elements to pages

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -77,15 +77,78 @@ angular.module('controllers', [])
     });
 })
 
-.controller('ListCtrl', function($scope, $ionicLoading) {
+.controller('ListCtrl', function($scope) {
     console.log('ready');
 })
 
-.controller('AddCtrl', function($scope, $ionicLoading, uiGmapGoogleMapApi, $stateParams) {
-    $scope.title = $stateParams.title;
-    console.log('ready');
+.controller('AddCtrl', function($scope, $ionicHistory, $ionicLoading, uiGmapGoogleMapApi) {
+    $scope.mapReady = false;
+    $scope.map = {
+        center: {
+            latitude: 0,
+            longitude: 0
+        },
+        events: {
+            dragstart: function(map) {
+                console.log('what a drag...');
+            }
+        },
+        options: {
+            disableDefaultUI: true
+        },
+        zoom: 6
+    };
+
+    uiGmapGoogleMapApi.then(function(uiMap) {
+        $scope.mapReady = true;
+        $scope.centerOnMe();
+    });
+
+    $scope.centerOnMe = function() {
+        console.log("Centering");
+        $scope.loading = $ionicLoading.show({
+            content: 'Getting current location...',
+            showBackdrop: false
+        });
+        navigator.geolocation.getCurrentPosition(function(pos) {
+            console.log('Got pos', pos);
+            $scope.map.center.latitude = pos.coords.latitude;
+            $scope.map.center.longitude = pos.coords.longitude;
+            $scope.map.position = {
+                id: 'position',
+                coords: {
+                    latitude: pos.coords.latitude,
+                    longitude: pos.coords.longitude
+                }
+            };
+            $scope.map.zoom = 17;
+            $ionicLoading.hide();
+        }, function(error) {
+            alert('Unable to get location: ' + error.message);
+        });
+    };
+
+    $scope.$on('g-places-autocomplete:select', function(event, place) {
+        $scope.loading = $ionicLoading.show({
+            content: 'Getting location...',
+            showBackdrop: false
+        });
+        var lat = place.geometry.location.lat();
+        var lon = place.geometry.location.lng();
+        $scope.map.center.latitude = lat;
+        $scope.map.center.longitude = lon;
+        $scope.map.search = {
+            id: 'search',
+            coords: {
+                latitude: lat,
+                longitude: lon
+            }
+        };
+        $scope.map.zoom = 17;
+        $ionicLoading.hide();
+    });
 })
 
-.controller('ReportCtrl', function($scope, $ionicLoading) {
+.controller('ReportCtrl', function($scope) {
     console.log('ready');
 });
