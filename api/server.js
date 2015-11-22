c0c1991076af93ebfa167c2e6b68d7bcaf41c9d6
Andrew Nelson <andy@andyhub.com>
2015-11-22 13:40:57 -0500

    started on db model

--- api/server.js
+++ api/server.js
@@ -1,5 +1,3 @@
-// server.js
-
 // BASE SETUP
 // =============================================================================
 
@@ -7,6 +5,9 @@
 var express    = require('express');        // call express
 var app        = express();                 // define our app using express
 var bodyParser = require('body-parser');
+var mongoose   = require('mongoose');
+var model     = require('./model.js');
+mongoose.connect('mongodb://localhost/mobile-map-io'); // connect to our database
 
 // configure app to use bodyParser()
 // this will let us get the data from a POST
@@ -18,6 +19,13 @@ var port = process.env.PORT || 8080;        // set our port
 // ROUTES FOR OUR API
 // =============================================================================
 var router = express.Router();              // get an instance of the express Router
+
+// middleware to use for all requests
+router.use(function(req, res, next) {
+    // do logging
+    console.log('Something is happening.');
+    next(); // make sure we go to the next routes and don't stop here
+});
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/', function(req, res) {
