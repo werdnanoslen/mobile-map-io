f59a6147e539c1cdefc6e00534d0983d8a9e6113
Andrew Nelson <andy@andyhub.com>
2016-02-04 13:47:17 -0500

    replaced deprecated call

--- api/server.js
+++ api/server.js
@@ -18,10 +18,10 @@ function allowCrossDomain(req, res, next) {
 
     // intercept OPTIONS method
     if ('OPTIONS' == req.method) {
-      res.send(200);
+        res.sendStatus(200);
     }
     else {
-      next();
+        next();
     }
 };
 
